e13a55858bf75825d30b68fd0bddd872140a31b2
lijanahmmed <lijanahmmed@gmail.com>
2025-09-30 21:43:57 +0600

    create react

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { Suspense } from 'react'
+import './App.css'
+import DaisyNav from './components/DaisyNav/DaisyNav'
+import NavBar from './components/NavBar/NavBar'
+import PricingOptions from './components/PricingOptions/PricingOptions'
+import ResultsChart from './components/ResultsChart/ResultsChart'
+import axios from 'axios'
+import MarksChart from './components/MarksChart/MarksChart'
+
+
+
+
+function App() {
+
+  return (
+    <>
+
+      
+      
+
+    </>
+  )
+}
+
+export default App
