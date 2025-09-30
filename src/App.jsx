9338c81ed84bbbef66a5408aa16eee5b98143b71
lijanahmmed <lijanahmmed@gmail.com>
2025-09-30 21:50:17 +0600

    added price card section

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import axios from 'axios'
 import MarksChart from './components/MarksChart/MarksChart'
 
 
-
+const pricingPromise = fetch('pricingData.json').then(res => res.json());
 
 function App() {
 
@@ -19,7 +19,12 @@ function App() {
         <NavBar></NavBar>
         {/* <DaisyNav></DaisyNav> */}
       </header>
-      
+      <main>
+        <Suspense fallback={<span className="loading loading-spinner loading-lg"></span>}>
+          <PricingOptions pricingPromise={pricingPromise}></PricingOptions>
+        </Suspense>
+
+      </main>
 
     </>
   )
