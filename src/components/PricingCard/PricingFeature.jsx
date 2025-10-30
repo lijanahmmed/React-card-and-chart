018e65dc4b5ed799fc273c34906927956bcc4f53
lijanahmmed <lijanahmmed@gmail.com>
2025-10-31 00:08:55 +0600

    all code in formate

--- src/components/PricingCard/PricingFeature.jsx
+++ src/components/PricingCard/PricingFeature.jsx
@@ -1,10 +1,13 @@
-import { CircleCheckBig } from 'lucide-react';
-import React from 'react';
+import { CircleCheckBig } from "lucide-react";
+import React from "react";
 
-const PricingFeature = ({feature}) => {
-    return (
-        <p className='flex mt-4'> <CircleCheckBig className='mr-2'></CircleCheckBig> {feature}</p>
-    );
+const PricingFeature = ({ feature }) => {
+  return (
+    <p className="flex mt-4">
+      {" "}
+      <CircleCheckBig className="mr-2"></CircleCheckBig> {feature}
+    </p>
+  );
 };
 
 export default PricingFeature;
--- src/components/PricingOptions/PricingOptions.jsx
+++ src/components/PricingOptions/PricingOptions.jsx
@@ -1,31 +1,19 @@
-import React, { use } from 'react';
-import PricingCard from '../PricingCard/PricingCard';
-import DaisyPricing from '../DaisyPricing/DaisyPricing';
+import React, { use } from "react";
+import DaisyPricing from "../DaisyPricing/DaisyPricing";
 
-const PricingOptions = ({pricingPromise}) => {
+const PricingOptions = ({ pricingPromise }) => {
+  const pricingData = use(pricingPromise);
 
-    const pricingData = use(pricingPromise);
-
-
-    return (
-        <div>
-            <h2 className='text-5xl'>Get our Membership</h2>
-            <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-8'>
-                {/* {
-                    pricingData.map(pricing => <PricingCard 
-                        key={pricing.id}
-                        pricing={pricing}></PricingCard>)
-                } */}
-                    {
-                        pricingData.map(pricing => <DaisyPricing
-                            key={pricing.id}
-                            pricing={pricing}
-                        ></DaisyPricing>)
-                    }
-
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <h2 className="text-5xl">Get our Membership</h2>
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+        {pricingData.map((pricing) => (
+          <DaisyPricing key={pricing.id} pricing={pricing}></DaisyPricing>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default PricingOptions;
